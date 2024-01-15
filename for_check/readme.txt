105ad354d9fe4d3f987d8bafc681c2fc18e6a81b
zaqmond <zaqmond@yandex.ru>
2024-01-15 17:48:23 +0300

    add script count chars, add quiet flag in bouth scripts

--- for_check/readme.txt
+++ for_check/readme.txt
@@ -2,7 +2,3 @@ openssl genrsa -out privatekey 2048                                             
 openssl dgst -sha256 -sign privatekey -out readme.signature readme.txt          подпись
 openssl rsa -in privatekey -outform PEM -pubout -out publickey                  генерация открытого ключа
 openssl dgst -sha256 -verify publickey -signature readme.signature readme.txt   для проверки
-
-
-
-
--- merge_files.sh
+++ merge_files.sh
@@ -1,12 +1,25 @@
 #!/bin/sh
 
-if [ $# -ne 2 ]; then
-    echo "Использование: $0 файл_источник(откуда) файл_назначение(куда)\nисточник будет скопирован в конец файла назначения"
+quiet=false
+
+while [ "$#" -gt 0 ]; do
+    case "$1" in
+        -q|--quiet)
+            quiet=true
+            shift
+            ;;
+        *)
+            source_file="$1"
+            destination_file="$2"
+            shift 2
+            ;;
+    esac
+done
+
+if [ -z "$source_file" ] || [ -z "$destination_file" ]; then
+    echo "Использование: $0 [-q|--quiet] файл_источник(откуда) файл_назначение(куда)\nисточник будет скопирован в конец файла назначения"
     exit 1
 fi
-
-source_file="$1"
-destination_file="$2"
 
 # Проверка существования файла_источника
 if [ ! -e "$source_file" ]; then
@@ -21,5 +34,6 @@ fi
 
 # Добавление содержимого файла_источника в конец файла_назначения
 cat "$source_file" >> "$destination_file"
-
+if [ "$quiet" = false ]; then
 echo "Содержимое файла '$source_file' успешно добавлено в конец файла '$destination_file'."
+fi
